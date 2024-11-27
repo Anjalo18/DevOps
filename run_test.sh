01e90c517e4345c145ecbaedf4910726268f38d3
Anjalo18 <angelo.bongiovi@edu.ecole-89.com>
2024-11-27 17:29:39 +0100

    Modification ajouté le 27/11/24 17:29

--- run_test.sh
+++ run_test.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 git checkout dev
-
+echo "-----------------------------$(date '+%d/%m/%y %H:%M')----------------------------"
+echo "-----------------------------Début des Tests---------------------------"
 echo "Execution des tests, Vérification de la version de python"
 python3 --version
+echo "Le 1er test est réussi."
 
 if [ $? -ne 0 ]; then
 	echo "Erreur : Python n'est pas installé, le test a échoué."
@@ -11,31 +13,34 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "Execution d'un second test..."
-python3 -c "print('test réussi!')"
+python3 -c "print('Affichage via print !')"
 
-echo "Le test réussie."
-git status
+echo "Le 2e test est réussi."
+echo "------------------------------Fin des Tests-----------------------------"
+echo "SUCCESS"
 
-if [ -n "$(git status --porcelain)" ]; then
-	echo "Des modifications ont été détectées."
-	git add .
-	git commit -m "Modification ajouté le $(date '+%d/%m/%y %H:%M')"
-else
-	echo "Aucune Modification."
-fi
+#git status
 
-git checkout staging
-git merge dev
+#if [ -n "$(git status --porcelain)" ]; then
+#	echo "Des modifications ont été détectées."
+#	git add .
+#	git commit -m "Modification ajouté le $(date '+%d/%m/%y %H:%M')"
+#else
+#	echo "Aucune Modification."
+#fi
 
-echo "Etat du dépôt après merge :"
-git status
+#git checkout staging
+#git merge dev
 
-git push origin staging
+#echo "Etat du dépôt après merge :"
+#git status
+
+#git push origin staging
  
 
-if [ $? -eq 0 ]; then
-	echo "Push réussi vers la branche staging."
-else
-	echo "Erreur : le push a échoué."
-	exit 1
-fi
+#if [ $? -eq 0 ]; then
+#	echo "Push réussi vers la branche staging."
+#else
+#	echo "Erreur : le push a échoué."
+#	exit 1
+#fi
