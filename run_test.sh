0931803004bdcd4e924df2c566704355dae14072
Anjalo18 <angelo.bongiovi@edu.ecole-89.com>
2024-11-27 15:08:45 +0100

    Modification du 27/11/2024

---
+++ run_test.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+git checkout dev
+
+echo "Execution des tests, Vérification de la version de python"
+python3 --version
+
+if [ $? -ne 0 ]; then
+	echo "Erreur : Python n'est pas installé, le test a échoué."
+	exit 1
+fi
+
+echo "Execution d'un second test..."
+python3 -c "print('test réussi!')"
+
+echo "Le test réussie."
+git status
+
+if [ -n "$(git status --porcelain)" ]; then
+	echo "Des modifications ont été détectées."
+	git add .
+	git commit -m "Modification du 27/11/2024"
+else
+	echo "Aucune Modification."
+fi
+
+git checkout staging
+git merge dev
+
+echo "Etat du dépôt après merge :"
+git status
+
+git push origin staging
+ 
+
+if [ $? -eq 0 ]; then
+	echo "Push réussi vers la branche staging."
+else
+	echo "Erreur : le push a échoué."
+	exit 1
+fi
---
+++ wsgi.py
@@ -0,0 +1,5 @@
+from mydevopspro import app
+
+if __name__ == "__main__":
+	app.run()
+
